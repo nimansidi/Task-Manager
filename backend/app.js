7fc3c59e5ab9d229ec57505490e76b9905ed5623
Abhinav Gautam <119486953+abh1nav9@users.noreply.github.com>
2024-06-23 23:15:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,14 @@ app.use(bodyParser.json());
 // MongoDB connection
 const mongoURI = 'mongodb+srv://abh1nav:Abh1nav%4009@taskmanager.9z2q7q7.mongodb.net/?retryWrites=true&w=majority'; // Ensure this URI is correct
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
-  })
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoURI);
+
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('MongoDB connected');
+//     app.listen(5000, () => console.log('Server running on port 5000'));
+//   })
+//   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 const taskRoutes = require('./routes/tasks');
